765e43bc6b28bc8e8ce794f5d87f17aa8442cf15
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2023-03-02 16:30:48 +0100

    Starter Code

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <StepperComponent/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import StepperComponent from './components/StepperComponent.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    StepperComponent,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
