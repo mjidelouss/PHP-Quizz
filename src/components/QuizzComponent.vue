2e9a9d45af68f18fabe860391cdf9461e304554d
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2023-03-12 11:31:34 +0100

    loading questions and options completed

---
+++ src/components/QuizzComponent.vue
@@ -0,0 +1,193 @@
+<script setup>
+/* eslint-disable */
+import { ref, computed } from 'vue'
+// import { questions } from "../data.js";
+const questions = ref([
+  {
+	question: 'What is Vue?',
+	answer: 0,
+	options: [
+		'A framework',
+		'A library',
+		'A type of hat'
+	],
+	selected: null
+  },
+  {
+	question: 'What is Vuex used for?',
+	answer: 2,
+	options: [
+		'Eating a delicious snack',
+		'Viewing things',
+		'State management'
+	],
+	selected: null
+  },
+  {
+	question: 'What is Vue Router?',
+	answer: 1,
+	options: [
+		'An ice cream maker',
+		'A routing library for Vue',
+		'Burger sauce'
+	],
+	selected: null
+  }
+])
+const quizCompleted = ref(false)
+const currentQuestion = ref(0)
+const shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+
+const score = computed(() => {
+	let value = 0
+	shuffledQuestions.value.map(q => {
+		if (q.selected != null && q.answer == q.selected) {
+			console.log('correct');
+			value++
+		}
+	})
+	return value
+})
+const getCurrentQuestion = computed(() => {
+	let question = shuffledQuestions.value[currentQuestion.value]
+	question.index = currentQuestion.value
+	return question
+})
+const SetAnswer = (e) => {
+	shuffledQuestions.value[currentQuestion.value].selected = e.target.value
+	e.target.value = null
+}
+const NextQuestion = () => {
+	if (currentQuestion.value < shuffledQuestions.value.length - 1) {
+		currentQuestion.value++
+		return
+	}
+	quizCompleted.value = true
+}
+
+</script>
+
+<template>
+    <div v-if="!quizCompleted">
+  <!-- QUIZZ CONTAINER -->
+  <div class="container rounded p-3">
+        <div class="justify-center flex-column">
+          <div id="box">
+            <div class="box-item">
+              <p id ="progressText" class="box-para fw-bold">
+                {{ `Question ❔ ` }}
+              </p>
+              <div id="progressBar">
+                <div id="progressBarFull"></div>
+              </div>
+            </div>
+            <div class="box-item">
+              <p class="box-para fw-bold">Timer ⏳</p>
+              <h1 class="box-text" id="timer">{{  }}</h1>
+            </div>
+            <div class="box-item">
+              <p class="box-para fw-bold">
+                Score 🎯
+              </p>
+              <h1 class="box-text" id="score">{{ score }}</h1>
+            </div>
+          </div>
+          <h3 id="question" class="mb-4 mt-3">{{ getCurrentQuestion.question }}</h3>
+          <div class="options">
+				<label 
+					v-for="(option, index) in getCurrentQuestion.options" 
+					:for="'option' + index" 
+					:class="`option ${
+						getCurrentQuestion.selected == index 
+							? index == getCurrentQuestion.answer 
+								? 'correct' 
+								: 'wrong'
+							: ''
+					} ${
+						getCurrentQuestion.selected != null &&
+						index != getCurrentQuestion.selected
+							? 'disabled'
+							: ''
+					}`">
+					<input 
+						type="radio" 
+						:id="'option' + index" 
+						:name="getCurrentQuestion.index" 
+						:value="index" 
+						v-model="getCurrentQuestion.selected" 
+						:disabled="getCurrentQuestion.selected"
+						@change="SetAnswer" 
+					/>
+					<span>{{ option }}</span>
+				</label>
+			</div>
+			
+			<button 
+                id="qst-btn"
+				@click="NextQuestion" 
+				:disabled="!getCurrentQuestion.selected">
+				{{ 
+					getCurrentQuestion.index == questions.length - 1 
+						? 'Finish' 
+						: getCurrentQuestion.selected == null
+							? 'Select an option'
+							: 'Next question'
+				}}
+			</button>
+          
+        </div>
+      </div>
+      <section v-if="quizCompleted">
+			<h2>You have finished the quiz!</h2>
+			<p>Your score is {{ score }}/{{ questions.length }}</p>
+		</section>
+    </div>
+</template>
+    
+<style lang="scss">
+.options {
+	margin-bottom: 1rem;
+}
+.option {
+	padding: 1rem;
+	display: block;
+	background-color: #b394e0;
+	margin-bottom: 0.5rem;
+	border-radius: 0.5rem;
+	cursor: pointer;
+}
+.option:hover {
+	background-color: #96cce1;
+}
+.option.correct {
+	background-color: #2cce7d;
+}
+.option.wrong {
+	background-color: #ff5a5f;
+}
+.option:last-of-type {
+	margin-bottom: 0;
+}
+.option.disabled {
+	opacity: 0.5;
+}
+.option input {
+	display: none;
+}
+#qst-btn {
+	appearance: none;
+	outline: none;
+	border: none;
+	cursor: pointer;
+	padding: 0.5rem 1rem;
+	background-color: #010905;
+	color: #ffffff;
+	font-weight: 700;
+	text-transform: uppercase;
+	font-size: 1.2rem;
+	border-radius: 0.5rem;
+}
+#qst-btn:disabled {
+	opacity: 0.5;
+}
+</style>
---
+++ src/components/RulesComponent.vue
@@ -0,0 +1,40 @@
+<script>
+    export default {
+      data () {
+        return {
+        }
+      },
+    }
+</script>
+
+<template>
+  <div>
+   <h1 class="display-2 lh-1 text-center" style="margin-top: 2rem; color: black;">PHP Knowledge Test</h1>
+        <div class="mt-4" id="container">
+            <div class="ms-4" id="">
+                  <h2 class="text-center fw-bold text-black mt-2">RULES</h2>
+                  <h5 class="text-black text-center fw-bold mt-2">💡 The contestant have to answer 12 Questions in PHP.</h5>
+                  <h5 class="text-black text-center fw-bold">💡 Each question has a time limit of 30 seconds</h5>
+                  <h5 class="text-black text-center fw-bold">💡 With each correct question you get points.</h5>
+                  <h5 class="text-center text-black fw-bold mt-2">  😁😁 Good Luck 😁😁</h5>
+            </div>
+        <img id="quiz" style="margin-left: 20rem;" src="../assets/quizz.png" alt="" height="410" width="520">
+        </div>
+      </div>
+</template>
+    
+<style lang="scss">
+  #container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  
+  h1, p {
+    margin-right: 20px;
+  }
+  
+  #quiz {
+    float: right;
+  }
+</style>
